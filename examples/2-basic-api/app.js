7a18ef57cf99dc750ab0e277f3b34d8a755b0872
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-03-14 15:13:07 +0530

    exmaples 1 and 2

---
+++ examples/2-basic-api/app.js
@@ -0,0 +1,28 @@
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios.get('/api/2-basic-api');
+    const products = data
+      .map((item) => {
+        const {
+          image: { url },
+          name,
+          price,
+        } = item;
+        return `<article class="product">
+          <img src="${url}" alt=${name} />
+          <div class="info">
+            <h5>${name}</h5>
+            <h5 class="price">$${price}</h5>
+          </div>
+        </article>`;
+      })
+      .join('');
+    result.innerHTML = products;
+  } catch (error) {
+    console.log(error.response.data);
+  }
+};
+
+fetchData();
